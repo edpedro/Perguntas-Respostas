3e365fb24867a869b41f8bc6dc65702fde0b7448
edp2019 <edp2013.edp2013.ep@gmail.com>
2019-10-03 21:13:36 -0300

    Formulario pronto

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require("express");
 const app = express();
-
+const bodyParser = require("body-parser");
+//utilizando EJS
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-
+//Body Parse
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+//Rotas
 app.get("/", (req, res) => {
   res.render("index");
 });
 
 app.get("/perguntar", (req, res) => {
   res.render("perguntar");
+});
+
+app.post("/salvarpegunta", (req, res) => {
+  var tit = req.body.titulo;
+  var des = req.body.descricao;
+
+  res.send(`formulario recebindo:  Titulo: ${tit} e Descrição ${des} `);
 });
 
 app.listen(4000, () => {
